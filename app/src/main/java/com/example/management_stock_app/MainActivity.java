5b56c2bff4876bef1d0bdbcc607e765fe17d611a
soyidwahyud <soyidwahyu4@gmail.com>
2019-11-26 21:29:21 +0700

    Add Splash Screen

--- app/src/main/java/com/example/management_stock_app/MainActivity.java
+++ app/src/main/java/com/example/management_stock_app/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_container, new LoginFragment())
---
+++ app/src/main/java/com/example/management_stock_app/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.management_stock_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private int waktu_loading=4000;
+    String isLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home =new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
